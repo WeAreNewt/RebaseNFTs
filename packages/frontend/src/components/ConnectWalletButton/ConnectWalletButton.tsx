ff4b84f5536315b3336ae974611f689399644a2d
Rootul Patel <rootulp@gmail.com>
2022-03-14 23:43:56 -0400

    feat: connect to Metamask
    Fixes https://github.com/WeAreNewt/RebaseNFTs/issues/15

--- packages/frontend/src/components/ConnectWalletButton/ConnectWalletButton.tsx
+++ packages/frontend/src/components/ConnectWalletButton/ConnectWalletButton.tsx
@@ -1,3 +1,33 @@
+import { useWeb3React } from "@web3-react/core"
 import { Button } from '^@components'
+import { injectedConnector } from "^@services/connector"
 
-export const ConnectWalletButton = () => <Button text="Connect Wallet" />
+export const ConnectWalletButton = () => {
+  const { active, activate, deactivate, error } = useWeb3React()
+
+  async function connect() {
+      try {
+          await activate(injectedConnector)
+      } catch (e) {
+          console.error(e)
+      }
+  }
+
+  async function disconnect() {
+      try {
+          deactivate()
+      } catch (e) {
+          console.log(e)
+      }
+  }
+
+  return (
+    <>
+    {active ? (
+      <Button onClick={disconnect} variant="secondary" text="Disconnect" />
+    ) : (
+      <Button onClick={connect} variant="primary" text={"Connect"}/>
+    )}
+    </>
+  )
+}
