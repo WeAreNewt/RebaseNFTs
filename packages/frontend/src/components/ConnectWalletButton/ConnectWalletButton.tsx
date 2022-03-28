b66d07adaf8d982e78c1416438bd65886dc8791d
arttu <arttu@Arttu-MacBook-Pro.local>
2022-03-28 13:31:43 +0300

    Bracket missing

--- packages/frontend/src/components/ConnectWalletButton/ConnectWalletButton.tsx
+++ packages/frontend/src/components/ConnectWalletButton/ConnectWalletButton.tsx
@@ -7,3 +7,4 @@ export const ConnectWalletButton: React.FC  = () => {
     return (
         <Button onClick={connect} disabled={isActive ? true : false} text={isActive ? "Connected" : "Connect wallet"} />
     )
+    }
