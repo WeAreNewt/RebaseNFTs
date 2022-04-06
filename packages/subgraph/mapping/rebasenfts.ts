c413c1cdab176973595314d6130423abe635cb14
Arttu <arttu@AM-C4R27ML6TL.local>
2022-04-06 11:57:00 +0300

    subgraph init

---
+++ packages/subgraph/mapping/rebasenfts.ts
@@ -0,0 +1,12 @@
+import { Stake } from '../generated/schema'
+import { StakeCall } from '../generated/RebaseNfts/RebaseNfts'
+import { log } from '@graphprotocol/graph-ts'
+
+export function handleStake(event: StakeCall): void {
+    // let stake = new Stake('123')
+    
+    // stake.amount = BigInt.fromI32(9999)
+    // stake.timestamp = BigInt.fromI32(9999)
+    // stake.save()
+}
+
